a785065c793381c2b42f169b01a62b30f4e4c3e9
Andy Pellett <anrope@gmail.com>
2014-01-28 08:16:05 -0500

    initial playerjs stuff working, playing a specific clip of video

--- js/soundboardcreator.js
+++ js/soundboardcreator.js
@@ -1,1 +1,35 @@
-alert('lets create some soundboards');
+window.addEventListener('message', function (a, b, c) {
+  console.log('window message listener', a, b, c)
+})
+
+window.setTimeout(function () {
+  console.log(document.getElementsByTagName('iframe')[0].id)
+}, 1000);
+
+var playById = function(id, start, end) {
+  var embed = document.getElementById(id).contentWindow;
+  var embedDomain = 'http://cdn.embedly.com';
+  
+  // set up a listener so we can hear about events happening with the embed
+  embed.postMessage(JSON.stringify({
+    method: 'addEventListener',
+    value: 'event'
+  }), embedDomain);
+
+  // set the duration and start playing
+  embed.postMessage(JSON.stringify({
+    method: 'setCurrentTime',
+    value: start
+  }), embedDomain);
+  embed.postMessage(JSON.stringify({
+    method: 'play'
+  }), embedDomain);
+
+  // stop the embed once we hit our end time
+  window.setTimeout(function () {
+    console.log('setTimeout pausing', (end - start) * 1000, id);
+    embed.postMessage(JSON.stringify({
+      method: 'pause'
+    }), embedDomain);
+  }, (end - start) * 1000);
+}
