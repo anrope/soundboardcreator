72a7c65a19cd0eeed7b4dc1c3b2ce5da1dc740b7
agibson <art@embed.ly>
2014-02-04 11:41:50 -0500

    add save mode for boards

--- js/soundboardcreator.js
+++ js/soundboardcreator.js
@@ -58,6 +58,136 @@ var playSlot = function(slot, start, end) {
   }
 }
 
+///////////////////////
+//Setup Board from hash
+///////////////////////
+
+var setupSlot = function(data) {
+    if (data.html) {
+      this.html(data.html);
+      this.attr("data-html", data.html);
+      this.attr("data-title", data.title);
+      this.attr("data-description", data.description);
+    }
+    return false;
+}
+
+var setupBoard = function() {
+  var hash = window.location.hash;
+  var slot_hash = "";
+  if (hash) {
+    hash = hash.substring(1, hash.length);
+    hash = atob(hash);
+    items = hash.split(",");
+    for (var i=0;i<items.length;i++) {
+      slot_hash = items[i];
+      console.log("setting up slot:" + slot_hash);
+      var slot_vals = slot_hash.split(";");
+      var slot = slot_vals[0];
+      var url = decodeURIComponent(slot_vals[1]);
+      var start = slot_vals[2].split(":")[0];
+      var end = slot_vals[2].split(":")[1];
+      window.current_slot = slot;
+      $("." + slot).attr("data-url", url);
+      callEmbedly(url, setupSlot.bind($("." + slot)));
+      setTimeSlot(start, end);
+    }
+  }
+}
+
+/////////////////
+//Slot Creation
+/////////////////
+var callEmbedly = function (url, cb) {
+  var call = "http://api.embed.ly/1/oembed?width=400&scheme=http&url=";
+  $.getJSON(call + encodeURIComponent(url) + "&callback=?", cb);
+}
+
+var createSlot = function (data) {
+  if (data.html) {
+    //slot1 should be removed for something generic
+    $("." + window.current_slot).html(data.html);
+    $("." + window.current_slot).attr("data-html", data.html);
+    $("." + window.current_slot).attr("data-title", data.title);
+    $("." + window.current_slot).attr("data-description", data.description);
+    $(".url-input-mod").hide();
+    $(".time-input-mod").show();
+  } else { 
+    $("." + window.current_slot).html("<button>Try Again?</button>");
+  }
+}
+
+// adds URL data from Embedly
+var addURL =  function($input, $button){
+  if($input.length !=0){
+    $input.on('focus', function(){
+      $(this).val("");
+      $(this).attr("enabled", true);
+    })
+
+    $button.on('click', function(){
+      var url = $input.val();
+      $input.val("");
+      //do something with data
+      console.log("current slot: " + window.current_slot)
+      $("." + window.current_slot).attr("data-url", url);
+      data = callEmbedly(url, createSlot);
+      
+      return false;
+    });
+  }
+}
+
+var setTimeSlot = function(start, end) {
+        //load data into slot div
+      $("." + window.current_slot).attr("data-start", start);
+      $("." + window.current_slot).attr("data-end", end);
+
+      //onclick
+      $("#" + window.current_slot + " button").on('click', function(){
+        moveToHugeSlot(this.parentNode);
+        playSlot(window.current_slot, start, end);
+      });
+      $("#" + window.current_slot + " button").text(  "Play"); 
+      
+      $(".time-input-mod").hide();
+}
+
+// Adds start, end time from user
+var addTime = function($input1, $input2, $button){
+
+    $button.on('click', function(){
+      var start = $input1.val();
+      var end = $input2.val();
+      $input1.val("");
+      $input2.val("");
+
+      //load data into slot div
+      setTimeSlot(start, end);
+      $(".time-input-mod").hide();
+
+      return false;
+    });
+}
+
+var saveBoard = function () {
+  var board_list = [];
+  for (var i=1;i<11;i++) {
+    if ($('.slot' + i)[0].hasAttribute("data-url")) {
+      var time = $('.slot' + i).attr("data-start") 
+            + ":" + $('.slot' + i).attr("data-end");
+      var url = encodeURIComponent($('.slot' + i).attr("data-url")) + "";
+      console.log("saving url and time:" + time + "," + url);
+      board_list.push("slot" + i + ";" + url + ";" + time);
+    }
+  }
+  console.log("plaintext hash:" + board_list.join(","));
+  hash = btoa(board_list.join(","));
+  var board_link = window.location.href.split("#")[0] + "#" + hash;
+  $(".link").html('<a target="_blank" href="' + board_link + '">Saved Board</>');
+  console.log("creating board link:" + board_link);
+}
+
 $(document).ready(function () {
   setupButtonListeners();
 
@@ -69,80 +199,11 @@ $(document).ready(function () {
     console.log('window message listener', a, b, c)
   })
 
-
-  /////////////////
-  //Slot Creation
-  ///////////////
-    var callEmbedly = function (url, cb) {
-      var call = "http://api.embed.ly/1/oembed?width=400&scheme=http&url=";
-      $.getJSON(call + encodeURIComponent(url) + "&callback=?", cb);
-    }
-
-    var createSlot = function (data) {
-      if (data.html) {
-        //slot1 should be removed for something generic
-        $("." + window.current_slot).html(data.html);
-        $("." + window.current_slot).attr("data-html", data.html);
-        $("." + window.current_slot).attr("data-title", data.title);
-        $("." + window.current_slot).attr("data-description", data.description);
-        $(".url-input-mod").hide();
-        $(".time-input-mod").show();
-      } else { 
-        $("." + window.current_slot).html("<button>Try Again?</button>");
-      }
-    }
-
-    // adds URL data from Embedly
-    var addURL =  function($input, $button){
-      if($input.length !=0){
-        $input.on('focus', function(){
-          $(this).val("");
-          $(this).attr("enabled", true);
-        })
-
-        $button.on('click', function(){
-          var url = $input.val();
-          $input.val("");
-          //do something with data
-          console.log("current slot: " + window.current_slot)
-          $("." + window.current_slot).attr("data-url", url);
-          data = callEmbedly(url, createSlot);
-          
-          return false;
-        });
-      }
-    }
-
-    // Adds start, end time from user
-    var addTime = function($input1, $input2, $button){
-
-        $button.on('click', function(){
-          var start = $input1.val();
-          var end = $input2.val();
-          $input1.val("");
-          $input2.val("");
-
-          //load data into slot div
-          $("." + window.current_slot).attr("data-start", start);
-          $("." + window.current_slot).attr("data-end", end);
-
-          //onclick
-          $("#" + window.current_slot + " button").on('click', function(){
-            moveToHugeSlot(this.parentNode);
-            playSlot(window.current_slot, start, end);
-          });
-          $("#" + window.current_slot + " button").text(  "Play"); 
-          
-          $(".time-input-mod").hide();
-
-          return false;
-        });
-    }
-
     //setup forms
     var current_slot;
     var players;
     addURL($("#url-input"),$(".button",".url-input-mod"));
     addTime($("#time-input-1"),$("#time-input-2"),$(".button",".time-input-mod"));
     $(".save-mod button").on('click', saveBoard);
+    setupBoard();
 });
